63e3b3ec6d671ea1e02bbad353a14f37469b64f2
skycong <ssskycong@gmail.com>
2026-07-03 12:29:24 +0800

    chore: 删除 vite-plugin-mock 相关配置

--- config/vite/plugins/index.ts
+++ config/vite/plugins/index.ts
@@ -6,14 +6,11 @@ import type { Plugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import windiCSS from 'vite-plugin-windicss'
-import VitePluginCertificate from 'vite-plugin-mkcert'
 import vueSetupExtend from 'vite-plugin-vue-setup-extend'
 import { ConfigSvgIconsPlugin } from './svgIcons'
 import { AutoRegistryComponents } from './component'
 import { AutoImportDeps } from './autoImport'
-import { ConfigMockPlugin } from './mock'
 import { ConfigVisualizerConfig } from './visualizer'
-import { ConfigCompressPlugin } from './compress'
 import { ConfigRestartPlugin } from './restart'
 import { ConfigProgressPlugin } from './progress'
 
@@ -46,9 +43,6 @@ export function createVitePlugins(isBuild: boolean) {
 
   // vite-plugin-svg-icons
   vitePlugins.push(ConfigSvgIconsPlugin(isBuild))
-
-  // vite-plugin-mock
-  vitePlugins.push(ConfigMockPlugin(isBuild))
 
   // rollup-plugin-visualizer
   vitePlugins.push(ConfigVisualizerConfig())
